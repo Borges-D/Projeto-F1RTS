035c1711c3ecfed26408e4fcbacfc98ec1ad498d
Borges-D <davidcontatodev@gmail.com>
2024-05-26 21:41:31 -0300

    Anatomia das Calsses - 1

---
+++ java-anatomia/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+//Palavras compostar a inicial tem que ser maiuscula
+
+public class MinhaClasse { //Corpo da Classe
+    public static void main(String[] args) { //Corpo do psvm
+        System.out.print ( "Ola gente, sejam bem-vindos" );
+            
+    }
+}
