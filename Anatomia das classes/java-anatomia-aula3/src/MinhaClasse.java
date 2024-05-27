4b92fc826f55107b033a91e50fa7f193327fdbf2
Borges-D <davidcontatodev@gmail.com>
2024-05-26 23:03:25 -0300

    Anatomia de Classes terminadas

--- Anatomia das classes/java-anatomia-aula3/src/MinhaClasse.java
+++ Anatomia das classes/java-anatomia-aula3/src/MinhaClasse.java
@@ -1,33 +1,28 @@
 public class MinhaClasse {
-   
 
     public static void main(String[] args) {
 
         String primeiroNome = "David";
         String segundoNome = "Lucas";
-
         String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
-
         System.out.println(nomeCompleto);
-        //String meuNome = 'Lucas'; //Var atribuicao
+        // String meuNome = 'Lucas'; //Var atribuicao
 
-        //int anoNascimento = 2000; // Var do tipo inteiro
+        // int anoNascimento = 2000; // Var do tipo inteiro
 
-       // boolean verdadeira = true;  // Var representa simNao, true e false 
+        // boolean verdadeira = true; // Var representa simNao, true e false
 
         // anoNascimento = 2001; //Continua com var do tipo, so mudou o valor
 
-
-
         // Metodos com dois parametros
-        //  int somar (int numeroUm, int numero2)
+        // int somar (int numeroUm, int numero2)
 
+        // String formatarCep (long cep)
 
-        //  String formatarCep (long cep)
-        
     }
-    public static String nomeCompleto (String primeiroNome, String segundoNome) {
-            return "Resultado do metodo = " + primeiroNome.concat(" ").concat(segundoNome);
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do metodo = " + primeiroNome.concat(" ").concat(segundoNome);
     }
-    
+
 }
---
+++ Anatomia das classes/java-anatomia-aula6/src/minhaClasse.java
@@ -0,0 +1,5 @@
+public class minhaClasse {
+    //Apenas explicando sobre o recomendado para o java sobre identação basica
+    // Falando sobre deixar em apenas uma lingua o codigo para ficar bem identado
+    
+}
